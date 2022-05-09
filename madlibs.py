de7130f79d30fce1be375674ddbc3042cf2e8b42
biochmst <jessica.leger@ontariotechu.net>
2022-05-09 18:00:18 -0400

    first draft

---
+++ madlibs.py
@@ -0,0 +1,21 @@
+print ("Welcome to mad libs! Please enter a value for the prompts below:")
+
+adj1 = input("adjective: ")
+noun1 = input("noun: ")
+noun2 = input("noun: ")
+name = input("friend name: ")
+verb1 = input("past tense verb: ")
+verb2 = input("past tense verb: ")
+verb3 = input("verb: ")
+noun3 = input("noun: ")
+
+madlib = (f"Camping is so {adj1}! Last year, I saw a {noun1} climb out of a \
+tree and steal our {noun2}. {name} ran after the {noun1} to get it back, but by \
+the time they got there it had already {verb1} it! Everyone {verb2} along the \
+water's edge until the sun came up, at which point it was time to {verb3}. I'll \
+never forget my time at {noun3} Lake!")
+
+print(madlib)
+
+
+
